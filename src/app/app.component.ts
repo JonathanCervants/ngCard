24796d08df13d8e5590b6732cbdb7e65f1b74012
jcervants <jesus.cervantes@kurax.dev>
2025-03-13 17:18:27 -0500

    add dashboard footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./ui/header/header.component";
+import { SiderbarComponent } from "./ui/siderbar/siderbar.component";
+import { FooterComponent } from "./ui/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SiderbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
