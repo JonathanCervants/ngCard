22e6487ba3d3be5cf33617e09d07e3e8b37d9e66
jcervants <jesus.cervantes@kurax.dev>
2025-03-14 17:51:35 -0500

    añadiendo httpclient

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private service:AuthService)
 
 }
