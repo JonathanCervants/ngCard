ac8e544afd8ff2030c5158a3d0b2135eae82e881
jcervants <jesus.cervantes@kurax.dev>
2025-03-17 17:45:29 -0500

    añadiendo authguard

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { config } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,32 +7,14 @@ import { config } from 'rxjs';
 export class AuthService {
 
   constructor(private http:HttpClient) { 
-    http.get('https://fakestoreapi.com/products').subscribe(config=>{
-      console.log(config)
-      //a need a token with url
+    http.get('/api').subscribe(config=>{
+
     })
   }
-
   
-  private setToken(){
-    token = this.tokenKey;
-  }
-  
-  private getToken():string | null{
-    return localStorage.getItem(this.tokenKey)
-  }
 
-  isAuthenticad():any{
-    const token = this.getToken()
-    if (!token){
-      return false
-    }
-    const payload = 
-    const expect
-    return Date
-  }
 
- logout():void{
-  localStorage.removeItem(this.tokenKey) 
-  }
+  isAuthenticad(){}
+
+
 }
