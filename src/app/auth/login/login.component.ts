cc71e7b7a66f0c48dba584d39b81ce05d4efa919
jcervants <jesus.cervantes@kurax.dev>
2025-04-25 17:41:28 -0500

    f

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginForm?: any
+  formGroup = new FormGroup({
+    username : new FormControl('', Validators.required),
+    password :new FormControl('', Validators.required)
+  });
 
   constructor(private service:AuthService){
     ngOnInit:void{
-
     }
   }
  
   onSubmit(){
+    console.warn(this.formGroup.value)
     // if(this.formLogin.valid){
     //   capturo valores
     //   if(){
