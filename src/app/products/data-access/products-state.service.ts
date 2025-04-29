bfcae594fc6c0c752a58e0658d29d0ac353f98d8
Your Name <you@example.com>
2025-04-29 07:23:15 -0500

    states service entendiedno

--- src/app/products/data-access/products-state.service.ts
+++ src/app/products/data-access/products-state.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { Product } from "../interfaces/product.interface";
+import { ProductsService } from "./products.service";
 
 interface State{
   products: Product[]
@@ -8,15 +9,29 @@ interface State{
 
 @Injectable()
 export class ProductsStateService{
+  private productService = inject(ProductsService)
   private initialState: State = {
-    this.subscribeb()
+    products : [],
+    status:  'loading' as const
   }
 
-  loadProducts = this.changePage$.pipe(
-
-  )
-
   state = signalSlice({
-    
+    initialState = this.initialState,
+    sources: [
+      this.productService.getProducts().pipe(map())
+    ]
   })
 }
+
+// 
+// Se tiene que escalar usar api gateway
+// Implementar sistemas resilientes.
+// Monolitico -> Service Oriented -> Microservicios
+// Dependiendo un fallo podría afectar toda la aplicación, 
+// Comunicación de eventos, dependencia baja
+// Controllers C# ajustar carga para el inicio 
+// Jenkins: Pipelines, cypress, AudioListener
+// IC: preparar un release de Software ASAP
+// EC: automatizar el proceso de despliguege con interval. humana
+// DC: automatizar el respliguege continuo sin hinter humana.
+// Conceptos: Jobs.
